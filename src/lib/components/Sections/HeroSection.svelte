167751ceafc24e90e8d33aaf8bccd81136055885
G. Troy Johnson <github@weave.sh>
2025-04-27 20:34:51 -0700

    HeroSection and Button

---
+++ src/lib/components/Sections/HeroSection.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  import { Button } from "$components";
+
+  function onclick() {
+    goto("/#contact-form");
+  }
+</script>
+<section class="hero-section default-margin">
+  <h3>Hello! I'm Troy</h3>
+  <div class="underscore mb-m"></div>
+  <h1>I started writing code at a young age</h1>
+  <h1 class="dark-grey mb-m"> and then learned to run systems later.</h1>
+  <Button {onclick}>Let's talk</Button>
+</section>
+<style>
+  .hero-section {
+    padding-top: 60px;
+  }
+</style>
