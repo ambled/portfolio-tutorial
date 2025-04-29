c8ead2e652f8512196dbe90b5b2cb382b999a857
G. Troy Johnson <github@weave.sh>
2025-04-29 10:30:26 -0700

    Sanity setup for WorkSection projects

--- src/lib/utils/sanity.ts
+++ src/lib/utils/sanity.ts
@@ -1,4 +1,5 @@
 import {createClient, type ClientConfig} from '@sanity/client';
+import imageUrlBuilder from "@sanity/image-url";
 
 const config: ClientConfig = {
     projectId: 'mjpx7k9o',
@@ -9,3 +10,40 @@ const config: ClientConfig = {
 
 const sanityClient = createClient(config);
 export default sanityClient;
+
+export function processProjectEntries(rawProject: SanityProject) {
+    const builder = imageUrlBuilder(sanityClient);
+    const projectImageUrl = builder.image(rawProject.image).url();
+    const processedProject: ProcessedProject = {
+        name: rawProject.name,
+        company: rawProject.company,
+        dateAccomplished: rawProject.dateAccomplished,
+        stack: rawProject.stack,
+        projectImageUrl,
+        slug: rawProject.slug,
+        content: rawProject.content.map(processProjectContent)
+      
+    }
+
+    return processedProject;
+}
+
+function processProjectContent(content: RawTextContent | RawImageContent) {
+    if (content._type === 'block') {
+        const processedTextContent: ProcessedTextContent = {
+            type: 'text',
+            style: content.style,
+            textToRender: content.children.map(elem => elem.text).join('\n'),
+        }
+        return processedTextContent;
+    } else {
+        const builder = imageUrlBuilder(sanityClient);
+        const projectImageUrl = builder.image(content).url();
+        const processedImage: ProcessedImageContent = {
+            type: "image",
+            url: projectImageUrl,
+        };
+        return processedImage;
+    }
+    
+}
