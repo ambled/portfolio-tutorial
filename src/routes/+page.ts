bec961bf1785607f2c7d6edfe0c76a2bbdb5d8ae
G. Troy Johnson <github@weave.sh>
2025-04-29 11:33:00 -0700

    MyWorkSection

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,12 +1,20 @@
-import type { DevExperience } from '$lib/types/sanity';
-import sanityClient from '$lib/utils/sanity';
+
+import sanityClient, { processProjectEntries } from '$lib/utils/sanity';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ params }) => {
-    const workExperience: DevExperience[] = await sanityClient.fetch(
+    const workExperience: SanityWorkExperience[] = await sanityClient.fetch(
         '*[_type == "devExperience"] | order(startDate desc)');
 
+    const rawProjects: SanityProject[] = await sanityClient.fetch(
+        '*[_type == "project"] | order(dateAccomplished desc)');
+
+
+    const projects = rawProjects.map(processProjectEntries)
+
+    console.log(projects[0])
+
     return {
-        workExperience
+        workExperience, projects
     };
 }
